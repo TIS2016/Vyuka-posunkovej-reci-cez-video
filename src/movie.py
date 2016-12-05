251578c28d18c5def452a042b972113e980f2558
pe-ge <gergelp@gmail.com>
2016-12-05 11:51:46 +0100

    refactoring

--- src/movie.py
+++ src/movie.py
@@ -3,21 +3,21 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 
-def frame_to_image(arr):
+def frame_to_image(frame):
     mode = 'RGB'
-    height, width, channels = arr.shape
-    arr = arr.reshape(width * channels, height)
-    return Image.frombuffer(mode, (width, height), arr.tostring(), 'raw', mode, 0, 1)
+    height, width, channels = frame.shape
+    frame = frame.reshape(width * channels, height)
+    return Image.frombuffer(mode, (width, height), frame.tostring(), 'raw', mode, 0, 1)
 
 
 window = tk.Tk()
 canvas = tk.Canvas(window, width=1280, height=720)
 canvas.pack()
 
-
 video = VideoFileClip('aa.mp4')
 num_frame = 20
 frame = video.get_frame(num_frame)
 image = ImageTk.PhotoImage(frame_to_image(frame))
 image_id = canvas.create_image(400, 400, image=image)
+
 window.mainloop()
