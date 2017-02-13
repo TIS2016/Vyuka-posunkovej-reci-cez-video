4a01fd647078fcdbf81bbe7426fc06193da0fe26
bohunicka14 <bohunicka.ingrid@gmail.com>
2017-02-13 16:57:31 +0100

    Add files via upload

---
+++ install.cmd
@@ -0,0 +1,10 @@
+python-3.5.3rc1.exe
+set PATH=%PATH%;C:\Python35\Scripts
+pip install python-vlc
+pip install moviepy
+pip install opencv_python-3.1.0-cp35-cp35m-win32.whl
+pip install numpy-1.11.2+mkl-cp35-cp35m-win32.whl
+pip install Pillow
+plugin_ffmpeg_install.py
+
+
